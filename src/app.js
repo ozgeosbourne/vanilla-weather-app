12cfca90b7c8689bb8eb53abbc7f3661942c1247
ozgeosbourne <ozgenurayyasar@gmail.com>
2022-10-14 22:02:28 +0300

    Added humidity - wind ids and functions

--- src/app.js
+++ src/app.js
@@ -4,11 +4,15 @@ function displayTemperature(response) {
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = response.data.name;
-
+    let descriptionElement = document.querySelector("#description");
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    //let humidityElement = document.querySelector("#humidity");
+    //humidityElement.innerHTML = response.data.main.humidity;
+    //let windElement = document.querySelector("#wind");
+    //windElement.innerHTML = response.data.wind.speed;
 }
 
 let apiKey = "1d038ee28ef2727a9f0310860ac10ae9";
 let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
