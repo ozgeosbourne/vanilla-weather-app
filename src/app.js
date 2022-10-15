08cebd5b6eae81829303c51e64f74bf9099a94bd
ozgeosbourne <ozgenurayyasar@gmail.com>
2022-10-15 22:11:58 +0300

    Activated the search engine

--- src/app.js
+++ src/app.js
@@ -32,9 +32,18 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", response.data.weather[0].description);
 
 }
-
+function search(city) {
 let apiKey = "1d038ee28ef2727a9f0310860ac10ae9";
-let city = "Istanbul";
 let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
 axios.get(apiUrl).then(displayTemperature);
+
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit",handleSubmit);
